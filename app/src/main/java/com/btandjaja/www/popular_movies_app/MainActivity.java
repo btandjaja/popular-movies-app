22bab05db5022d86b4b7161cf312a0da391b7f1a
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-04-30 15:02:52 -0700

    create menu/add settings (string)/populate menu

--- app/src/main/java/com/btandjaja/www/popular_movies_app/MainActivity.java
+++ app/src/main/java/com/btandjaja/www/popular_movies_app/MainActivity.java
@@ -2,6 +2,8 @@ package com.btandjaja.www.popular_movies_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* Menu */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_settings){
+            /* do something */
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
