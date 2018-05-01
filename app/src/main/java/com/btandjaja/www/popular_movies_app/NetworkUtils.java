9eb44c0a0f637da8d14cfba2c752a93fb19e4d09
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-05-01 12:26:47 -0700

    empty networkutil class

---
+++ app/src/main/java/com/btandjaja/www/popular_movies_app/NetworkUtils.java
@@ -0,0 +1,15 @@
+package com.btandjaja.www.popular_movies_app;
+
+/**
+ * These utilities will be used to communicate with the network.
+ */
+public class NetworkUtils {
+    /* base url & phone picture size */
+    final static String MOVIES_BASE_URL = "http://image.tmdb.org/t/p/";
+    final static String PHONE_BASE_SIZE = "w185";
+
+    final static String POPULAR_MOVIES_BASE_URL = "http://api.themoviedb.org/3/movie/popular?api_key=";
+    //TODO remove key prior submitting
+    final static String POPULAR_MOVIES_KEY = "20893aae2a9da0098c89e73e1dcad948";
+    
+}
