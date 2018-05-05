1219c5effb0c9ef6083a7665619343847d4e5bdd
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-05-05 16:02:58 -0700

    fixing MovieViewHolder class

--- app/src/main/java/com/btandjaja/www/popular_movies_app/MovieAdapter.java
+++ app/src/main/java/com/btandjaja/www/popular_movies_app/MovieAdapter.java
@@ -2,21 +2,25 @@ package com.btandjaja.www.popular_movies_app;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.NumberViewHolder>{
+import com.squareup.picasso.Picasso;
+
+public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
     private int mNumbersOfMovies;
     /* constructor */
     public MovieAdapter (int numOfMovies) { mNumbersOfMovies = numOfMovies; }
 
     @NonNull
     @Override
-    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
 
     }
 
@@ -25,7 +29,18 @@ public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.NumberViewH
         return 0;
     }
 
-    class NumberViewHolder extends RecyclerView.ViewHolder {
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+        /* image fill in gridview layout */
+        private ImageView mImageView;
+        private String LINK_ADDRESS = "http://image.tmdb.org/t/p/w185/";
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            /* get the view from image layout */
 
+            //mImageView = (ImageView) itemView.findViewById(R.id.iv_movie);
+        }
+        /* bind the image layout with the provided image */
+        void bind(String image) {
+            Picasso.with().load(LINK_ADDRESS + image).into(mImageView);}
     }
 }
