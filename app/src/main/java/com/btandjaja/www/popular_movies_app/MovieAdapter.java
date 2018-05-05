2bd7ba3d5436836ac261fbf5166a85d1a336069d
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-05-05 15:42:03 -0700

    added recommended functions, not implemented

--- app/src/main/java/com/btandjaja/www/popular_movies_app/MovieAdapter.java
+++ app/src/main/java/com/btandjaja/www/popular_movies_app/MovieAdapter.java
@@ -1,6 +1,31 @@
 package com.btandjaja.www.popular_movies_app;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
-public class MovieAdapter extends  RecyclerView{
+public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.NumberViewHolder>{
+    private int mNumbersOfMovies;
+    /* constructor */
+    public MovieAdapter (int numOfMovies) { mNumbersOfMovies = numOfMovies; }
+
+    @NonNull
+    @Override
+    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class NumberViewHolder extends RecyclerView.ViewHolder {
+
+    }
 }
