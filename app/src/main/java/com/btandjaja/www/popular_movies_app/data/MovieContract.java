a85a56f2411357af03e497df713d5efcc2f5e654
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-05-13 18:59:02 -0700

    remove comments

--- app/src/main/java/com/btandjaja/www/popular_movies_app/data/MovieContract.java
+++ app/src/main/java/com/btandjaja/www/popular_movies_app/data/MovieContract.java
@@ -8,17 +8,11 @@ public class MovieContract {
 
     public static final class MovieEntry implements BaseColumns {
         public static final String TABLE_NAME = "movies";
-        /* column 1 */
         public static final String COLUMN_NAME_TITLE = "original_title";
-        /* column 2 */
         public static final String COLUMN_NAME_POSTER_PATH = "poster_path";
-        /* column 3 */
         public static final String COLUMN_NAME_OVER_VIEW = "overview";
-        /* column 4 */
         public static final String COLUMN_NAME_RELEASE_DATE = "release_date";
-        /* column 5 */
         public static final String COLUMN_NAME_VOTE_AVERAGE = "vote_average";
-        /* column 6 */
         public static final String COLUMN_NAME_POPULARITY = "popularity";
     }
 }
