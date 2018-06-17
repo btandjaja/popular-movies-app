e10230db21bf0aa7e3b73168bcfad7f0ee90a393
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-06-17 16:56:42 -0700

    partial uri

--- app/src/main/java/com/btandjaja/www/popular_movies_app/data/MovieContentProvider.java
+++ app/src/main/java/com/btandjaja/www/popular_movies_app/data/MovieContentProvider.java
@@ -2,15 +2,32 @@ package com.btandjaja.www.popular_movies_app.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class MovieContentProvider extends ContentProvider{
+    public static final int MOVIES = 100;
+    public static final int MOVIE_WITH_ID = 101;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper mMovieDbHelper;
+
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        // directory
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_FAVORITE, MOVIES);
+        // single item
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_FAVORITE + "/#", MOVIE_WITH_ID);
+        return uriMatcher;
+    }
     @Override
     public boolean onCreate() {
-        return false;
+        Context context = getContext();
+        mMovieDbHelper = new MovieDbHelper(context);
+        return true;
     }
 
     @Nullable
--- app/src/main/java/com/btandjaja/www/popular_movies_app/data/MovieContract.java
+++ app/src/main/java/com/btandjaja/www/popular_movies_app/data/MovieContract.java
@@ -1,12 +1,20 @@
 package com.btandjaja.www.popular_movies_app.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class MovieContract {
+    public static final String AUTHORITY = "com.btandjaja.www.popular_movies_app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_FAVORITE = "favorite";
 
     public static final class MovieEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITE).build();
+
         public static final String TABLE_NAME = "favorite_movies";
         public static final String COLUMN_MOVIE_TITLES = "movie_title";
+        public static final String COLUMN_MOVIE_POSTER = "poster";
         public static final String COLUMN_MOVIE_ID = "id";
     }
 }
